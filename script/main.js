6f62c01fa9a8b22f82605617e1f8be5a216f435d
Darqoo <d.smolag@gmail.com>
2018-01-03 21:18:03 +0100

    #17 modyfied smooth scroll function

--- script/main.js
+++ script/main.js
@@ -32,10 +32,13 @@ $(document).ready(function() {
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top - 61
             }, 800, function() {
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
+                window.scrollTo(0,$(hash).offset().top - 61);
+
+
             });
         } // End if
     });
