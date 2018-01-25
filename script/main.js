afdaebedfaa7ff0dfe884b874f52a92ce1e00377
Darqoo <d.smolag@gmail.com>
2018-01-25 11:07:24 +0100

    adding loader page

--- script/main.js
+++ script/main.js
@@ -1,14 +1,20 @@
+window.addEventListener('load', function() {
+    $("#loader_bg").fadeOut();
+    setTimeout(
+        function() {
+            homePage();
+        }, 600);
+    var firstLi = document.getElementsByClassName('pagination')[0].firstChild.firstChild;
+    firstLi.addEventListener("click", function() {
+        setTimeout(homePage, 1000);
+    });
+});
+
 (function() {
     $(document).ready(function() {
-
-        // hide .navbar first
         $(".top_menu").hide();
-
-        // fade in .navbar
         $(function() {
             $(window).scroll(function() {
-
-                // set distance user needs to scroll before we start fadeIn
                 if ($(this).scrollTop() > 200) {
                     $('.top_menu').fadeIn();
                 } else {
@@ -21,26 +27,19 @@
 }());
 
 $(document).ready(function() {
-    // Add smooth scrolling to all links
     $("a").on('click', function(event) {
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            // Prevent default anchor click behavior
             event.preventDefault();
-            // Store hash
             var hash = this.hash;
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top - 61
             }, 800, function() {
-                // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
                 window.scrollTo(0, $(hash).offset().top - 61);
 
 
             });
-        } // End if
+        }
     });
 });
 
